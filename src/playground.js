7bd33a4ab3f6618db89b74fe22b311e183d55b1a
git-illite <71469298+git-illite@users.noreply.github.com>
2023-06-10 15:40:10 -0400

    incorporated tailwind and open sans font

---
+++ src/playground.js
@@ -0,0 +1,4 @@
+const fruits = ["Apple", "Banana", "Orange"];
+const vegetables = ["Cucumber", "Radish"];
+
+console.log([...fruits, ...vegetables]);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };

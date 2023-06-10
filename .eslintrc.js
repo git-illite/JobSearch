b7c08e669dbef27aa2f7c37b539f8392eee82f32
git-illite <71469298+git-illite@users.noreply.github.com>
2023-06-10 03:30:42 -0400

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "plugin:prettier/recommended",
   ],
@@ -17,10 +17,7 @@ module.exports = {
   },
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
       env: {
         jest: true,
       },

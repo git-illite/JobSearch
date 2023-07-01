733267aacec2ad0001bca503cb54134bb93763ca
git-illite <71469298+git-illite@users.noreply.github.com>
2023-07-01 00:24:35 -0400

    changed menuItems to an object,each item has a url

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -14,12 +14,15 @@
           <ul class="flex h-full p-0 m-0 list-none">
             <li
               v-for="menuItem in menuItems"
-              :key="menuItem"
+              :key="menuItem.text"
               class="h-full ml-9 first:ml-0"
               data-test="main-nav-list-item"
             >
-              <a href="" class="flex items-center h-full py-2.5">
-                {{ menuItem }}</a
+              <router-link
+                :to="menuItem.url"
+                class="flex items-center h-full py-2.5"
+              >
+                {{ menuItem.text }}</router-link
               >
             </li>
           </ul>
@@ -59,12 +62,12 @@ export default {
   data() {
     return {
       menuItems: [
-        "Teams",
-        "Locations",
-        "Life at Aden",
-        "How we hire",
-        "Students",
-        "Jobs",
+        { text: "Teams", url: "/" },
+        { text: "Locations", url: "/" },
+        { text: "Life at Aden", url: "/" },
+        { text: "How we hire", url: "/" },
+        { text: "Students", url: "/" },
+        { text: "Jobs", url: "/jobs/results/" },
       ],
       isLoggedIn: false,
     };

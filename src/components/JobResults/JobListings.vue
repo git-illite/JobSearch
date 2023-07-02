0c72e0fbb4bb34f0a5edb1f95d3ea22861d1a89d
git-illite <71469298+git-illite@users.noreply.github.com>
2023-07-02 03:37:51 -0400

    added previous and next page navigation

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -11,6 +11,20 @@
     <div class="mt-8 mx-auto">
       <div class="flex flex-row flex-nowrap">
         <p class="text-sm flex-grow">Page {{ currentPage }}</p>
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="previousPage"
+            :to="{ name: 'JobResults', query: { page: previousPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Previous</router-link
+          >
+          <router-link
+            v-if="nextPage"
+            :to="{ name: 'JobResults', query: { page: nextPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Next</router-link
+          >
+        </div>
       </div>
     </div>
   </main>
@@ -32,6 +46,16 @@ export default {
     };
   },
   computed: {
+    previousPage() {
+      const previousPage = this.currentPage - 1;
+      const firstPage = 1;
+      return previousPage >= firstPage ? previousPage : undefined;
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      const lastPage = this.jobs.length / 10;
+      return nextPage <= lastPage ? nextPage : undefined;
+    },
     currentPage() {
       const pageString = this.$route.query.page || "1";
       return Number.parseInt(pageString);

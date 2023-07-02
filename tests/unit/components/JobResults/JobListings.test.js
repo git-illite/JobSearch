9f4c423a5347c7394ea597f912cb35d19f46d78b
git-illite <71469298+git-illite@users.noreply.github.com>
2023-07-02 04:02:13 -0400

    added mockreset to test to avoid test pollution

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -1,14 +1,24 @@
-import { flushPromises, shallowMount } from "@vue/test-utils";
+import { flushPromises, shallowMount, RouterLinkStub } from "@vue/test-utils";
 import JobListings from "@/components/JobResults/JobListings.vue";
 import axios from "axios";
 
 jest.mock("axios");
 
 describe("JobListings", () => {
+  beforeEach(() => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+  });
+
+  afterEach(() => {
+    axios.get.mockReset();
+  });
   const createConfig = ($route) => ({
     global: {
       mocks: {
         $route,
+      },
+      stubs: {
+        "router-link": RouterLinkStub,
       },
     },
   });
@@ -21,14 +31,13 @@ describe("JobListings", () => {
   });
 
   it("fetches jobs", () => {
-    axios.get.mockResolvedValue({ data: [] });
     const $route = createRoute();
     shallowMount(JobListings, createConfig($route));
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
   it("creates a job listing for a max of 10 jobs", async () => {
-    axios.get.mockResolvedValue({ data: Array(10).fill({}) });
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
     const queryParams = {
       page: "1",
     };
@@ -37,5 +46,22 @@ describe("JobListings", () => {
     await flushPromises();
     const jobListings = wrapper.findAll("[data-test='job-listing']");
     expect(jobListings).toHaveLength(10);
+  });
+
+  describe("when query params exclude page number", () => {
+    it("it displays page 1", () => {
+      const queryParams = { page: undefined };
+      const $route = createRoute(queryParams);
+      const wrapper = shallowMount(JobListings, createConfig($route));
+      expect(wrapper.text()).toMatch("Page 1");
+    });
+  });
+  describe("when query params includes page number", () => {
+    it("it displays page number", () => {
+      const queryParams = { page: 3 };
+      const $route = createRoute(queryParams);
+      const wrapper = shallowMount(JobListings, createConfig($route));
+      expect(wrapper.text()).toMatch("Page 3");
+    });
   });
 });

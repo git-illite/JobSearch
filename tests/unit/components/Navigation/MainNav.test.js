615e2017935aae755fbbdb0ee846eae40fa8c016
git-illite <71469298+git-illite@users.noreply.github.com>
2023-07-03 01:34:09 -0400

    updated main nav test

--- tests/unit/components/Navigation/MainNav.test.js
+++ tests/unit/components/Navigation/MainNav.test.js
@@ -3,9 +3,11 @@ import { createStore } from "vuex";
 import MainNav from "@/components/Navigation/MainNav";
 
 describe("MainNav", () => {
-  const createConfig = (store) => ({
+  const createConfig = ($store) => ({
     global: {
-      plugins: [store],
+      mocks: {
+        $store,
+      },
       stubs: {
         "router-link": RouterLinkStub,
       },
@@ -13,14 +15,22 @@ describe("MainNav", () => {
   });
 
   it("displays company name", () => {
-    const store = createStore();
-    const wrapper = shallowMount(MainNav, createConfig(store));
+    const $store = {
+      state: {
+        isLoggedIn: false,
+      },
+    };
+    const wrapper = shallowMount(MainNav, createConfig($store));
     expect(wrapper.text()).toMatch("Aden Careers");
   });
 
   it("displays menu items for navigation", () => {
-    const store = createStore();
-    const wrapper = shallowMount(MainNav, createConfig(store));
+    const $store = {
+      state: {
+        isLoggedIn: false,
+      },
+    };
+    const wrapper = shallowMount(MainNav, createConfig($store));
     const navigationMenuItems = wrapper.findAll(
       "[data-test='main-nav-list-item']"
     );
@@ -36,41 +46,70 @@ describe("MainNav", () => {
   });
   describe("When user is logged out", () => {
     it("prompts user to sign in", () => {
-      const store = createStore();
-      const wrapper = shallowMount(MainNav, createConfig(store));
+      const $store = {
+        state: {
+          isLoggedIn: false,
+        },
+      };
+      const wrapper = shallowMount(MainNav, createConfig($store));
       const loginButton = wrapper.find("[data-test='login-button']");
       const profileImage = wrapper.findComponent("[data-test='profile-image']");
       expect(loginButton.exists()).toBe(true);
       expect(profileImage.exists()).toBe(false);
     });
+    it("issues call to Vuex to login user", async () => {
+      const commit = jest.fn();
+      const $store = {
+        state: {
+          isLoggedIn: false,
+        },
+        commit,
+      };
+      const wrapper = shallowMount(MainNav, createConfig($store));
+      const loginButton = wrapper.find("[data-test='login-button']");
+      await loginButton.trigger("click");
+      expect(commit).toHaveBeenCalledWith("LOGIN_USER");
+    });
   });
   describe("When user is logged in", () => {
     it("display users profile picture", () => {
-      const store = createStore({
-        state() {
-          return {
-            isLoggedIn: true,
-          };
+      const $store = createStore({
+        state: {
+          isLoggedIn: true,
         },
       });
-      const wrapper = shallowMount(MainNav, createConfig(store));
-
+      const wrapper = shallowMount(MainNav, createConfig($store));
       const profileImage = wrapper.find("[data-test='profile-image']");
       expect(profileImage.exists()).toBe(true);
     });
 
     it("displays subNav menu with additional info", () => {
-      const store = createStore({
+      const $store = createStore({
         state() {
           return {
             isLoggedIn: true,
           };
         },
       });
-      const wrapper = shallowMount(MainNav, createConfig(store));
-
+      const wrapper = shallowMount(MainNav, createConfig($store));
       const subnav = wrapper.find("[data-test='subnav']");
       expect(subnav.exists()).toBe(true);
+    });
+  });
+
+  describe("When user is logged in", () => {
+    it("issues call to Vuex to logout user", async () => {
+      const commit = jest.fn();
+      const $store = {
+        state: {
+          isLoggedIn: true,
+        },
+        commit,
+      };
+      const wrapper = shallowMount(MainNav, createConfig($store));
+      const logoutButton = wrapper.find("[data-test='profile-image']");
+      await logoutButton.trigger("click");
+      expect(commit).toHaveBeenCalledWith("LOGOUT_USER");
     });
   });
 });

2a2d6a3f6a8254c21eded68ba70d7775419a4905
git-illite <71469298+git-illite@users.noreply.github.com>
2023-07-02 23:36:46 -0400

    installed vuex

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import router from "@/router";
+import store from "./store";
 
 /* add icons to the library */
 library.add(faSearch);
 
 createApp(App)
+  .use(store)
   .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");

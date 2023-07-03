0ac3449d477a094e882c22f250fff25e2dac6637
git-illite <71469298+git-illite@users.noreply.github.com>
2023-07-03 00:11:42 -0400

    added index js test file

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,24 @@
 import { createStore } from "vuex";
 
-export default createStore({
-  state: {
+export const state = () => {
+  return {
     isLoggedIn: false,
+  };
+};
+
+export const mutations = {
+  LOGIN_USER(state) {
+    state.isLoggedIn = true;
   },
+};
+
+const store = createStore({
+  state,
   getters: {},
-  mutations: {
-    LOGIN_USER(state) {
-      state.isLoggedIn = true;
-    },
-  },
+  mutations,
   strict: process.env.NODE_ENV !== "production",
   actions: {},
   modules: {},
 });
+
+export default store;
---
+++ tests/unit/store/index.test.js
@@ -0,0 +1,18 @@
+import { state, mutations } from "@/store";
+
+describe("state", () => {
+  it("keeps track of whether user is logged in", () => {
+    const startingState = state();
+    expect(startingState.isLoggedIn).toBe(false);
+  });
+});
+
+describe("mutations", () => {
+  describe("LOGIN_USER", () => {
+    it("logs the user in", () => {
+      const state = { isLoggedIn: false };
+      mutations.LOGIN_USER(state);
+      expect(state).toEqual({ isLoggedIn: true });
+    });
+  });
+});

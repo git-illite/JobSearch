dc5028aa097d00666562f8c73e8a811ce102940d
git-illite <71469298+git-illite@users.noreply.github.com>
2023-07-14 16:09:17 -0400

    installed typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: "@typescript-eslint/parser",
   },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": "off",
   },
+
   overrides: [
     {
       files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
@@ -23,5 +22,12 @@ module.exports = {
         jest: true,
       },
     },
+  ],
+
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "@vue/typescript",
   ],
 };

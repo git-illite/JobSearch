f6b684f79057117559279475154b4aaba035acd5
git-illite <71469298+git-illite@users.noreply.github.com>
2023-07-23 22:37:46 -0400

    added computed method to component to update state

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="mt-5">
     <input
+      v-model="skillsSearchTerm"
       class="p-3 h-12 border border-solid border-brand-gray-1 shadow-gray rounded w-full text-base"
       type="text"
       placeholder="Computer Programming, Finance Degree"
@@ -9,9 +10,25 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { key } from "@/store";
+import { UPDATE_SKILLS_SEARCH_TERM } from "@/store/constants";
+import { computed, defineComponent } from "vue";
+import { useStore } from "vuex";
 
 export default defineComponent({
   name: "JobFiltersSidebarSkills",
+  setup() {
+    const store = useStore(key);
+    const skillsSearchTerm = computed({
+      get() {
+        return store.state.skillsSearchTerm;
+      },
+      set(value) {
+        store.commit(UPDATE_SKILLS_SEARCH_TERM, value);
+      },
+    });
+
+    return { skillsSearchTerm };
+  },
 });
 </script>
